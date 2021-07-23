33178e5ca802dc2466f3972788deb1732bfc2c35
Daniel Park <menoelpis@gmail.com>
2021-07-23 10:25:31 +0900

    Changed Deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,30 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.16.0"
 
+server '143.244.176.155', port: 22, roles: [:web, :app, :db], primary: true
 set :application, "myapp"
 set :repo_url, "git@github.com:menoelpis/myapp.git"
 
-# Deploy to the user's home directory
-set :deploy_to, "/home/deploy/#{fetch :application}"
+set :user, 'deploy'
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
+
+set :pty,             true
+set :use_sudo,        false
+set :stage,           :production
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/home/#{fetch(:user)}/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.access.log"
+set :puma_error_log,  "#{release_path}/log/puma.error.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+
+append :linked_files, "config/master.key"
 
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 
